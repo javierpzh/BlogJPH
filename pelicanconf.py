083b8a84af0b188252c023e1129bf6132e21cc68
javierpzh <reyole111@gmail.com>
2020-10-01 13:57:35 +0200

    añado archivos de pagina web

---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Javier Pérez Hidalgo'
+SITENAME = 'Blog Javier Pérez'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Madrid'
+
+DEFAULT_LANG = 'es'
+
+THEME = "/home/javier/virtualenv/Pelican/Blog/theme/pelican-clean-blog"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Instagram', 'https://www.instagram.com/javierpzh/'),
+         ('Twitter', 'https://twitter.com/jperezhid_'),
+         ('GitHub', 'https://github.com/javierpzh'),
+         )
+
+# Social widget
+SOCIAL = (('Instagram', 'https://www.instagram.com/javierpzh/'),
+         ('Twitter', 'https://twitter.com/jperezhid_'),
+         ('GitHub', 'https://github.com/javierpzh'),
+	 )
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True

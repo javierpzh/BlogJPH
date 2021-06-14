e6ce0146a196220baa0f6f97af1076530e3befe2
javierpzh <>
2021-06-14 11:30:06 +0200

    cambios

--- pelicanconf.py
+++ pelicanconf.py
@@ -22,15 +22,16 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Social widget
-SOCIAL = (('Instagram', 'https://www.instagram.com/javierpzh/'),
+SOCIAL = (
+         ('Instagram', 'https://www.instagram.com/javierpzh/'),
          ('Twitter', 'https://twitter.com/jperezhid_'),
          ('GitHub', 'https://github.com/javierpzh'),
-	 ('Facebook', 'https://www.facebook.com/javier.perezhidalgo.904'),
-	 ('envelope-square', 'mailto:javierperezhidalgo01@gmail.com'),
-	 )
+	     ('Facebook', 'https://www.facebook.com/javier.perezhidalgo.904'),
+	     ('envelope-square', 'mailto:javierperezhidalgo01@gmail.com'),
+         ('LinkedIn', 'https://www.linkedin.com/in/javier-p%C3%A9rez-hidalgo/')
+         )
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
-
